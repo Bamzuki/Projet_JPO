89eb87b34501375df566365d0cb35e7d2804b172
formation <formation@ubuntu.ubuntu-domain>
2019-04-10 15:50:30 +0200

    ANDROID Ajout de l'interactivitité sur carte

--- Explor_Descartes/app/src/main/java/eu/ensg/jpo/explor_descartes/donnesObjet/Batiment.java
+++ Explor_Descartes/app/src/main/java/eu/ensg/jpo/explor_descartes/donnesObjet/Batiment.java
@@ -2,19 +2,23 @@ package eu.ensg.jpo.explor_descartes.donnesObjet;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.Geometry;
 import com.mapbox.geojson.Polygon;
 import com.mapbox.geojson.gson.GeometryGeoJson;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.layers.FillLayer;
 import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.layers.PropertyValue;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.textField;
 
 public class Batiment extends DataBaseObject{
 
@@ -43,7 +47,9 @@ public class Batiment extends DataBaseObject{
             System.out.println(geometrie.toString());
             GeoJsonSource polygon = new GeoJsonSource("polygon"+this.id, geometrie.toString());
             style.addSource(polygon);
-            style.addLayer((new FillLayer("batiment"+this.id, "polygon"+this.id).withProperties(fillOpacity(0.5f), fillColor("blue"))));
+            FillLayer layerBatiment = new FillLayer("batiment"+this.id, "polygon"+this.id).withProperties(fillOpacity(0.5f), fillColor("blue"));
+            layerBatiment.withProperties(textField("geometry"));
+            style.addLayer(layerBatiment);
         }
 
     }
