2be761722f8e4edb07e9506470d6f987900ede06
shuthello <hugo.depaulis@gmail.com>
2019-04-25 14:39:14 +0200

    PHP & HTML Ajout du travail de Lucas et de la fonctionnalité mdp oublié

--- Interface_administrateur(avec-css)/image1.php
+++ Interface_administrateur(avec-css)/image1.php
@@ -1,12 +1,12 @@
 <?php
-//répertoire de déstination
-$target_dir = ".\image_ecole\'";
+  //Répertoire d'enregistrement
+  $target_dir = ".\image_ecole\'";
 	$target_file = $target_dir . basename($_FILES["fileselect"]["name"]);
 	//on initialise la variable update ok
 	$uploadOk = 1;
 	//on recup l'extention du fichier
 	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-	
+
 	//on a cliqué sur le bouton qui s'appel submit
 	if(isset($_POST["submit"])) {
 		//fichier image?
@@ -33,8 +33,8 @@ $target_dir = ".\image_ecole\'";
 		// $uploadOk = 0;
 	// }
 	// les formats autorisés
-	if($imageFileType != "jpg" &&$imageFileType != "JPG"&& $imageFileType != "png" && $imageFileType != "PNG" && $imageFileType != "jpeg" && $imageFileType != "JPEG" && $imageFileType != "gif" && $imageFileType != "GIF") {
-		
+	if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "PNG" && $imageFileType != "jpeg" && $imageFileType != "JPEG" && $imageFileType != "gif" && $imageFileType != "GIF") {
+
 		$message = "Les images doivent etre au format: JPG, JPEG, PNG ou GIF.";
 		echo $message;
 		$uploadOk = 0;
@@ -43,21 +43,21 @@ $target_dir = ".\image_ecole\'";
 	if ($uploadOk == 0) {
 		$message = "Erreur! impossible d'ajouter l'image.";
 		echo $message;
-		
+
 	// tt c'est bien passé
 	} else {
 		if (move_uploaded_file($_FILES["fileselect"]["tmp_name"], $target_file)) {
-			
-				
+
+
 				$message = "Image ajoutée avec succès.";
-					
+
 				echo $message;
-				
+
 		} else {
 			$message = "Erreur inconnue! Merci de retenter l'ajout plus tard ou de contacter l'administrateur.";
 			echo $message;
 		}
-	} 
-	
-	
+	}
+
+
 	?>
